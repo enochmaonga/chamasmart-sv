0b64dce3a8fae9f9484b124fe581bd69b26dacd8
Enoch Maonga Mokua <EMokua@safaricom.co.ke>
2025-08-19 16:53:44 +0300

    new url

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log('MongoDB connected');
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
+  app.listen(process.env.PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${process.env.PORT}`);
+});
 })
 .catch((err) => console.error('MongoDB connection error:', err));
