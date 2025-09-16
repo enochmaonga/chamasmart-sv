e83afc709518f3249cd8b803166f4296c7027171
Enoch Maonga Mokua <EMokua@safaricom.co.ke>
2025-09-16 12:04:38 +0300

    port issue

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log('MongoDB connected');
+  const PORT = process.env.PORT || 8001;
   app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${process.env.PORT}`);
 });
